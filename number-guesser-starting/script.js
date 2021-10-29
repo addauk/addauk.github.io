1189a554677d2e1a511f2ca256262c96d294e8db
addauk <77572184+addauk@users.noreply.github.com>
2021-10-29 14:32:14 +0100

    completed task

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -4,3 +4,25 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => Math.floor(Math.random() * 10);
+
+const getAbsoluteDistance = (num1,num2) => Math.abs(num1,num2);
+
+const compareGuesses = (userGuess,computerGuess,secretNumber) =>{
+    const userDistance = getAbsoluteDistance(userGuess - secretNumber)
+    const computerDistance = getAbsoluteDistance(computerGuess - secretNumber)
+    
+    return (computerDistance < userDistance) ?  false : true;
+}
+
+const updateScore = winner =>{
+
+    if(winner === 'human'){
+        humanScore++
+    }
+    if(winner === 'computer'){
+        computerScore++
+    }
+}
+
+const advanceRound = () => currentRoundNumber++;
